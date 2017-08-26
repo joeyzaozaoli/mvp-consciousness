7c9e938844d8d45fc56e86bfc92045d18c89b41c
Joey Li <joeyzaozaoli@gmail.com>
2017-08-26 11:42:33 -0700

    Serve up home.html and refactor using helper function #servePage

--- server.js
+++ server.js
@@ -3,19 +3,26 @@ var fs = require('fs');
 
 var headers = {};
 
+var servePage = function(pagePath, response) {
+  fs.readFile(pagePath, function(error, data) {
+    if (!error) {
+      var html = data.toString();
+      headers['Content-Type'] = 'text/html';
+      response.writeHead(200, headers);
+      response.end(html);
+    }
+  });
+};
+
 var requestHandler = function(request, response) {
   console.log(`Serving request method: ${request.method} & request url: ${request.url}`);
 
   if (request.method === 'GET') {
     if (request.url === '/') {
-      fs.readFile(__dirname + '/index.html', function(error, data) {
-        if (!error) {
-          var html = data.toString();
-          headers['Content-Type'] = 'text/html';
-          response.writeHead(200, headers);
-          response.end(html);
-        }
-      });
+      servePage(__dirname + '/index.html', response);
+
+    } else if (request.url === '/home') {
+      servePage(__dirname + '/home.html', response);
     }
   }
 };
