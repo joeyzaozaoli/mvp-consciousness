d7591a5f9ae71bbfd000fcc5fbdd52a2b4f0429b
Joey Li <joeyzaozaoli@gmail.com>
2017-08-26 11:25:59 -0700

    Serve up index.html via fs module

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 var http = require('http');
+var fs = require('fs');
 
-var handler = function(request, response) {
+var headers = {};
+
+var requestHandler = function(request, response) {
   console.log(`Serving request method: ${request.method} & request url: ${request.url}`);
+
+  if (request.method === 'GET') {
+    if (request.url === '/') {
+      fs.readFile(__dirname + '/index.html', function(error, data) {
+        if (!error) {
+          var html = data.toString();
+          headers['Content-Type'] = 'text/html';
+          response.writeHead(200, headers);
+          response.end(html);
+        }
+      });
+    }
+  }
 };
 
-var server = http.createServer(handler);
+var server = http.createServer(requestHandler);
 server.listen(3000, 'localhost');
 console.log('Listening on http://localhost:3000');
