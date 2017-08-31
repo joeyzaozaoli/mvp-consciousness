d6875ade94f51366fb0c17becaed6400db14a65b
Joey Li <joeyzaozaoli@gmail.com>
2017-08-31 09:05:35 -0700

    Render page using Express

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express');
-var bodyParser = require('body-parser');
+var parser = require('body-parser');
 
 var app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname));
+app.use(parser.json());
+app.use(parser.urlencoded({extended: true}));
 
 app.listen(3000, function() {
   console.log('Listening on http://localhost:3000');
 });
-
