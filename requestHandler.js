162b3a96e047ade9af0ee566a120707f7991a004
Joey Li <joeyzaozaoli@gmail.com>
2017-08-31 09:23:59 -0700

    Refactor request handler using Express

--- requestHandler.js
+++ requestHandler.js
@@ -1,26 +1,6 @@
-var helper = require('./helper');
-
-var requestHandler = function(request, response) {
-  console.log(`Serving request method: ${request.method} & request url: ${request.url}`);
-
-  if (request.method === 'GET') {
-    if (request.url === '/') {
-      helper.servePage(__dirname + '/index.html', response);
-
-    } else if (request.url === '/home') {
-      helper.servePage(__dirname + '/home.html', response);
-    }
-  }
-
-  if (request.method === 'POST') {
-    if (request.url === '/home') {
-      helper.gatherData(request, function(dataObj) {
-        // save dataObj
-        response.writeHead(302, {Location: '/home'});
-        response.end();
-      });
-    }
-  }
+var saveJournal = function(req, res) {
+  console.log(req.body);
+  res.redirect('/');
 };
 
-module.exports = requestHandler;
+module.exports = {saveJournal: saveJournal};
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var parser = require('body-parser');
 
+var handler = require('./requestHandler');
+
 var app = express();
 app.use(express.static(__dirname));
 app.use(parser.json());
 app.use(parser.urlencoded({extended: true}));
+
+app.post('/', handler.saveJournal);
 
 app.listen(3000, function() {
   console.log('Listening on http://localhost:3000');
