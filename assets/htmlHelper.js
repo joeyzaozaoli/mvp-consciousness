80893fc1d7b4d33efaee5782655d474b26490a2b
Joey Li <joeyzaozaoli@gmail.com>
2017-09-04 19:29:22 -0700

    Add CSS

--- assets/htmlHelper.js
+++ assets/htmlHelper.js
@@ -18,7 +18,7 @@ var generateQuote = function(quoteNode, authorNode) {
 
 var createGoalNode = function(num) {
   var tempNode = document.createElement('div');
-  var templateStr = `<textarea id='${num}' name='${num}'></textarea>`;
+  var templateStr = `<textarea id='${num}' class='goal-entry' name='${num}'></textarea>`;
   tempNode.innerHTML = templateStr;
   var templateNode = tempNode.childNodes[0];
   return templateNode;
@@ -29,8 +29,8 @@ var createGoalNode = function(num) {
 var createJournalNode = function(num) {
   var templateNode = document.createElement('tr');
   var templateStr = `
-      <td>${num}.</td>
-      <td><input type='text' id='${num}' name='${num}'></td>`;
+      <td class='journal-entry-number'>${num}.</td>
+      <td><input type='text' id='${num}' class='journal-entry' name='${num}'></td>`;
   templateNode.innerHTML = templateStr;
   return templateNode;
 };
