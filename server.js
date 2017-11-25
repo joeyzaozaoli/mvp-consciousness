c8f86352d17742b8fd692eccb4fe2a2b45b88835
Joey Li <joeyzaozaoli@gmail.com>
2017-11-25 11:24:46 -0800

    Use process.env.PORT

--- server.js
+++ server.js
@@ -52,6 +52,6 @@ app.post('/', handler.saveFormData);
   Server
 * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-app.listen(3000, function() {
-  console.log('Listening on http://localhost:3000');
+app.listen(process.env.PORT || 3000, function() {
+  console.log('Server listening');
 });
